89c977bd6b3a5861df1134a0254ae36171f965ed
Inna Huzhva <guzhva.inna@gmail.com>
2024-09-26 23:35:36 +0300

    Display country info more nicely

--- frontend/src/SelectedCountry.js
+++ frontend/src/SelectedCountry.js
@@ -16,12 +16,48 @@ function SelectedCountry({ country }) {
 
   return (
     <div>
-      <h1>Country Info</h1>
       {loadingStatus === "LOADING" && <div className="loader">Loading....</div>}
       {loadingStatus === "ERROR" && (
         <div className="loading-error">{loadingError}</div>
       )}
-      {loadingStatus === "SUCCESS" && <div>{JSON.stringify(countryInfo)}</div>}
+      {loadingStatus === "SUCCESS" && (
+        <div>
+          <h1>{country.name}</h1>
+          <div className="flag">
+            <img src={countryInfo.flag} alt="flag" height={100} />
+          </div>
+          <div className="borders">
+            <h2>Bordering countries</h2>
+            <ul>
+              {countryInfo.borderCountries.map((c) => (
+                <li key={c}>{c}</li>
+              ))}
+            </ul>
+            {countryInfo.borderCountries.length === 0 && (
+              <div>There is no neighbours</div>
+            )}
+          </div>
+          <div className="population">
+            <h2>Population data</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th>Year</th>
+                  <th>Count</th>
+                </tr>
+              </thead>
+              <tbody>
+                {countryInfo.population?.populationCounts.map((r) => (
+                  <tr key={r.year}>
+                    <td>{r.year}</td>
+                    <td>{r.value}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
