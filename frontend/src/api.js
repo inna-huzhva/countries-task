09d6404ffa3f1d2d9fc8ab108a353cb86deb9046
Inna Huzhva <guzhva.inna@gmail.com>
2024-09-26 22:55:07 +0300

    Fetch full country info

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -6,10 +6,35 @@ export async function getCountries() {
   return await response.json();
 }
 
-export async function getCountryInfo(country) {
+async function getBorderCountries(country) {
   const response = await fetch(`/api/borderCountries/${country.countryCode}`);
   if (!response.ok) {
     throw new Error("Network response was not ok");
   }
   return await response.json();
+}
+
+async function getCountryPopulation(country) {
+  const response = await fetch(`/api/countryPopulation/${country.name}`);
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return await response.json();
+}
+
+async function getCountryFlag(country) {
+  const response = await fetch(`/api/countryFlag/${country.name}`);
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return await response.json();
+}
+
+export async function getCountryInfo(country) {
+  const [borderCountries, population, flag] = await Promise.all([
+    getBorderCountries(country),
+    getCountryPopulation(country),
+    getCountryFlag(country),
+  ]);
+  return { borderCountries, population, flag };
 }
